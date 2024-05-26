c718808f557c3fc5efea967f86f399e4b659d105
Айзат Мадумарова <ajzatmadumarova@MacBook-Pro-Ajzat.local>
2024-05-26 20:30:26 +0600

    added route for rating a book

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
+import csv
 import numpy as np
 
 pt = pickle.load(open('pt.pkl', 'rb'))
 popular_df = pickle.load(open('popular.pkl', 'rb'))
 books = pickle.load(open('books.pkl', 'rb'))
 similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
+ratings_csv = 'Ratings.csv'
 
 app = Flask(__name__, template_folder='templates')
 
@@ -46,6 +48,34 @@ def recommend():
 
     return render_template('recommend.html', data=data)
 
+@app.route('/rate_book/<book_id>', methods=['POST'])
+def rate(book_id):
+    data = request.get_json()
+    if 'rating' not in data:
+        return jsonify({"error": "No rating provided"}), 400
+
+    try:
+        rating = int(data['rating'])
+    except ValueError:
+        return jsonify({"error": "Rating must be an integer"}), 400
+    
+    if is_book_rated(276724, book_id):
+        return jsonify({"error": "User has already rated this book"}), 400
+
+    with open(ratings_csv, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([276724, book_id, rating])
+    csvfile.close() 
+
+    return jsonify({"message": "Rating saved successfully", "User-ID": 276724, "ISBN": book_id, "Book-Rating": rating}), 201
+
+def is_book_rated(user_id, book_id):
+    with open(ratings_csv, 'r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if int(row['User-ID']) == user_id and row['ISBN'] == book_id:
+                return True
+    return False
 
 if __name__ == '__main__':
     app.run(debug=True)
